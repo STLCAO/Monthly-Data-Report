1c0c93bb68ca8c99d8d541d21051d4695efb999e
Gunathilake <gunathilakel@stlouiscao.org>
2019-04-05 17:08:01 -0500

    added some comments to intro and column with refusal rate

--- cleanFiles.R
+++ cleanFiles.R
@@ -1,30 +1,37 @@
 library(dplyr)
 
-#After getting a new file from a legal inquiry report add month column and get rid of group by column.
+#After getting a new file from a legal inquiry report-
+#add month column to each file and get rid of "groupby" column
+#Save all files in RawData folder
+
+
 setwd("C:/Users/Gunathilakel/Desktop/Monthly Data Report/Monthly-Data-Report/RawData")
 
-
-#While reading these files add a column that has a name 
 myMergedData <- 
   do.call(rbind,
           lapply(list.files(getwd()), read.csv,stringsAsFactors=FALSE))
-
-
 datain<-myMergedData%>%select(FileNumber,Stage,CaseStatus,NAME,Severity,Month)
+
 #Refused Cases
 datainRefused<-datain%>%filter(CaseStatus=='REFUSED',Stage!='CLOSED')%>%distinct(FileNumber,.keep_all = TRUE)
 refusedCases<-datainRefused%>%split(datainRefused$Month)
+
 #Issued Cases
 datainIssued<-datain%>%filter(CaseStatus=="OPEN")%>%distinct(FileNumber,.keep_all = TRUE)
 IssuedCases<-datainIssued%>%split(datainIssued$Month)
+
 #Review Cases
 datainReview<-datain%>%filter(CaseStatus=="REVIEW")%>%distinct(FileNumber,.keep_all = TRUE)
 ReviewedCases<-datainReview%>%split(datainReview$Month)
 
+#Creating Dataframe with all the Issued-Refused-Files
 refused<-sapply(refusedCases,function(l)(nrow(l)))
 issued<-sapply(IssuedCases,function(l)(nrow(l)))
 review<-sapply(ReviewedCases,function(l)(nrow(l)))
 accepted<-issued+review
 total<-refused+accepted
-data.frame(refused,accepted,total)
+RefusalRate<-(refused/total*100)
+data.frame(refused,accepted,total,RefusalRate)
+
+
 
